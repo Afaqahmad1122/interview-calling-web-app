a43067a57ba3785434dccde098c65d0c8a238071
Afaq Ahmad <afaqahmadnaeem@gmail.com>
2025-10-31 19:51:44 +0500

    Add start script to package.json and update server route handling

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,8 +10,8 @@ const __dirname = path.resolve();
 
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "frontend", "dist")));
-  app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
   });
 }
 
